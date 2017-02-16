56d1c17d4937d361f69fbed972012470d7c15cae
Henvertek <juanwfer@gmail.com>
2017-02-15 23:24:24 -0300

    Update Suma_de_digitos.ino

--- Suma_de_digitos.ino
+++ Suma_de_digitos.ino
@@ -1,5 +1,5 @@
 /*
-  Suma de dígitos https://circuits.io/circuits/4019650-the-unnamed-circuit/edit
+  Suma de dígitos  https://circuits.io/circuits/4019650-the-unnamed-circuit/edit
   
   El programa insta al usuario a introducir un número
   entero desde la consola. La función num acepta long
@@ -8,66 +8,65 @@
   No sé que tipo de error ocurriría si no se modifica
   el programa. La interfaz con el usuario sólo
   permite que éste ingrese números enteros, así que
-  el error no es un -1 devuelto por la consola, sino
+  el error no es un -1 devuelto por la función, sino
   un mensaje escrito para que se intente nuevamente.
+  
+  Si el número es mayor a 4294967295, el resultado de
+  la función será correcto, pero el serial mencionará
+  un número ingresado incorrecto debido a que decimal
+  empezó de 0. El resultado será correcto mientras
+  tenga hasta 32 cifras.
   
   Favor de leer todos los comentarios.
 */
-
+int candidate[32] = {};//32 cifras máximo.
+int lap = 0;//Contar las cifras ingresadas.
 void setup(){
   Serial.begin(9600);
-  user();//Posiblemente use esto bastante, así que lo convertí en una función para copypastear. 
-}
-
-void loop(){
-}
-
-int sum(unsigned long num){
-  String figures = String(num);//String es más amigable que char.
-  int answer = 0;//Para devolver.
-  for (int i = 0; i < sizeof(figures); i++){//Ciclo for que suma cada cifra de figures.
-    answer+=int(figures[i]);
-  }
-  return answer;
-}
-
-int clear(){//Limpia el buffer.
-  while(Serial.available() > 0)Serial.read();
-  return 0;//Porque pedí que una variable se resetee. candidate = clear();
-}
-
-void user(){//Utilizar el programa mediante la consola.
-  unsigned long candidate = 0;//Unsigned long para poder usar números grandes.
+  unsigned long decimal = 0;//Para mostrar al usuario lo que ingresó.
   Serial.println("Ingrese un numero entero seguido de un punto.\n >>>");
 
   while(true){//Hasta los break.
     if(Serial.available() > 0){
       int input = Serial.read();//Caracter ingresado.
-
+      
       if(input == '.'){//El usuario terminó de escribir el número.
-        if(candidate < 1){//Escribió o "0." o ".", nada interesante pasa con eso.
-          Serial.println("0? Eso no va a pasar. Intente nuevamente.\n >>>");//Error
-          candidate = clear();//En caso de que haya sido ".5468."
-        }
-
-        else{   
           Serial.print("El numero es "); 
-          Serial.print(candidate); 
+          Serial.print(decimal); 
           Serial.println(".");//Se le informa el número.
           break;
-        }
       }
       else if(input >= '0' && input <= '9'){//El usuario introdujo un número.
-        candidate *= 10;
-        candidate += input-48;//Mucho mejor que el array y las potencias. No es idea propia.
+        candidate[lap] = input-48;//El número a computar se completa.
+        decimal*=10;
+        decimal+=input-48;//El número a comunicar al usuario se completa. 
+        lap++;
       }
       else{
         Serial.print(input);
         Serial.println("? That ain't no number, try again m8 \n >>>");//Error.
-        candidate = clear();//En caso de que haya sido "59e31."
+        decimal = clear();//Limpiar el serial y hacer que decimal sea igual a 0.
+        lap = 0;//El array empezará a sobreescribirse.
       }
     }
   }
-  Serial.print("Los digitos del numero ");Serial.print(candidate);Serial.println(" suman:");
+  Serial.print("Los digitos del numero ");Serial.print(decimal);Serial.println(" suman:");//Este mensaje tiene sentido mientras se respete el tamaño máximo del unsigned long.
   Serial.println(sum(candidate));
+}
+
+void loop(){
+}
+
+int sum(int num[]){
+  int answer = 0;//Para devolver.
+  for (int i = 0; i < lap; i++){//Ciclo for que suma cada cifra de num.
+    answer += num[i];
+  }
+  return answer;
+}
+
+int clear(){
+  delay(10);//delay para que se ejecute el while. Esperemos no rebalsar el buffer en 10 milisegundos.
+  while(Serial.available() > 0)Serial.read();
+  return 0;
 }
